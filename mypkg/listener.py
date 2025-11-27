f5e06b22eb09c53326c849ff78efb1f6416d9182
robosys2025 <muuuuu2525@users.noreply.github.com>
2025-11-27 12:44:36 +0900

    add service and launch

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,14 +1,30 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
-
-rclpy.init()
-node = Node("listener")
-
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+from person_msgs.srv import Query
 
 def main():
-    pub = node.create_subscription(Int16, "countup", cb, 10)
-    rclpy.spin(node)
+    rclpy.init()
+    node = Node("listener")
+    client = node.create_client(Query, 'query')
+    
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('待機中')
+
+    req = Query.Request()
+    req.name = "浅野真夢"
+
+    future = client.call_async(req)
+
+    while rclpy.ok():
+        rclpy.spin_once(node) 
+        if future.done():     
+            try:
+                response = future.result()
+            except:
+                node.get_logger().info('呼び出し失敗')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
+                break 
+
+    node.destroy_node()
+    rclpy.shutdown()
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,21 +1,17 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.srv import Query
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
+def cb(request, response):
+    if request.name == "浅野真夢":
+        response.age = 19
+    else:
+        response.age = 255
 
-
-def cb():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
-
+    return response
 
 def main():
-    node.create_timer(0.5, cb)
+    rclpy.init()
+    node = Node("talker")
+    srv = node.create_service(Query, "query", cb) # サービスの作成
     rclpy.spin(node)
