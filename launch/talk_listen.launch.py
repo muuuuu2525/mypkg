87362ff7504d85082e1f7690801622ff302dd7c9
robosys2025 <muuuuu2525@users.noreply.github.com>
2025-12-04 13:04:49 +0900

    add test setting

---
+++ launch/talk_listen.launch.py
@@ -0,0 +1,16 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='mypkg',
+            executable='talker',
+        ),
+        launch_ros.actions.Node(
+            package='mypkg',
+            executable='listener',
+        ),
+    ])
