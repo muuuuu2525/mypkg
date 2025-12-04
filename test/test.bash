ffdc1fbfd5125c3d0b809f010dbf3593b441a21e
robosys2025 <muuuuu2525@users.noreply.github.com>
2025-12-04 12:44:59 +0900

    add test setting

--- test/test.bash
+++ test/test.bash
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 dir=~
-[ "$1" != "" ] && dir="$1"
+[ "$1" != "" ] && dir="$1"   # 引数があれば、それをホームディレクトリとして扱う
 
 cd $dir/ros2_ws
-colcon build
-source $dir/ros2_ws/install/setup.bash
+colcon build                 # ワークスペースでビルド
+source $dir/.bashrc          # 設定を読み込み
 
-timeout 10 ros2 run mypkg talker &
-timeout 10 ros2 run mypkg listener > /tmp/mypkg.log
+# ノードを10秒間実行し、ログを /tmp/mypkg.log に保存
+# ※注意: ここで talk_listen.launch.py を使用しています
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
+# ログの中に 'Listen: 10' があるか確認（あれば終了ステータス0＝成功）
 cat /tmp/mypkg.log | grep 'Listen: 10'
